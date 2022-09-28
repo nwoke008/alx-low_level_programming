fa9e04cbf12d4597c90aaec4f7b5ab45a018a26b
Uchenna Nwoke <uchennae.nwoke@gmail.com>
2022-09-28 06:08:45 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
+	return (0);
 	else
 	return (1 + _strlen_recursion(s + 1));
 }
@@ -26,11 +26,11 @@ int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
-		if (n1 == n2 || n1 == n2 + 1)
-		return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
+	if (n1 == n2 || n1 == n2 + 1)
+	return (1);
+	return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
-		return (0);
+	return (0);
 }
 
 /**

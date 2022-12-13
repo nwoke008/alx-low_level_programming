90583b7910d6e8ed716ce13a2dcd20ee1108ed66
Uchenna Nwoke <uchennae.nwoke@gmail.com>
2022-12-12 21:55:21 -0800

    4-isalpha.c

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _isalpha - Checks for Alphabetic Characters
+ *  @c: Alphabetic character
+ *  Return: 1 (Success) OR 0 (Failure)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
